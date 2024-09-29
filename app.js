82d6c443767a1002bf6f845190a83ed611778d3b
Payal <bordepayal26@gmail.com>
2024-09-29 19:37:39 +0530

    given logic for admissions

--- app.js
+++ app.js
@@ -48,3 +48,15 @@ window.onclick = function (event) {
         navLinks.classList.remove('active');
     }
 };
+
+// const navLinks = document.querySelectorAll('nav a');
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         const targetId = this.getAttribute('href');
+//         document.querySelector(targetId).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
